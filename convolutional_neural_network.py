7874b5d6887fafa850b4004094adea63b4bba5bf
chintiahildayanti <151912076+chintiahildayanti@users.noreply.github.com>
2025-04-25 14:29:38 +0700

    update

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -6,7 +6,7 @@ from sklearn.metrics import classification_report, confusion_matrix, ConfusionMa
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Conv2D, AveragePooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.models import load_model
 import random
 from tensorflow.keras.optimizers import Adam
@@ -61,20 +61,22 @@ validation_generator = datagen.flow_from_directory(
 # CNN Model
 model = Sequential([
     Conv2D(16, (3, 3), activation='relu', input_shape=(224, 224, 3)),
-    MaxPooling2D(2, 2),
+    AveragePooling2D(2, 2),
     Conv2D(32, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
+    AveragePooling2D(2, 2),
     Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
+    AveragePooling2D(2, 2),
     Conv2D(128, (3, 3), activation='relu'),
-    MaxPooling2D(2, 2),
+    AveragePooling2D(2, 2),
+    Conv2D(256, (3, 3), activation='relu'),
+    AveragePooling2D(2, 2),
     Flatten(),
     Dense(128, activation='relu'),
     Dropout(0.5),
     Dense(3, activation='softmax')
 ])
 
-model.compile(optimizer=Adam(learning_rate=1e-4),
+model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
